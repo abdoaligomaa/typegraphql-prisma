d480d2bbbc451b891e4998cf22e2a41ad3ad2659
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-10-07 00:52:08 +0200

    .

--- server.ts
+++ server.ts
@@ -2,17 +2,25 @@
 import "reflect-metadata";
 
 import {ApolloServer} from 'apollo-server'
+import { PrismaClient } from "@prisma/client";
 import {buildSchema} from "type-graphql"
 import { resolvers } from "@generated/type-graphql";
+
+const prisma=new PrismaClient({})
 
 const buildschema=async ()=>{
     const schema = buildSchema({ resolvers });
     return schema
 }
+ const context = () => {
+   return {
+     prisma,
+   };
+ };
 
 const app=async()=>{
     const schema=await buildschema()
-    const server=new ApolloServer({schema})
+    const server=new ApolloServer({schema,context})
     server.listen(8080,()=>console.log('server is running at port 8080'))
 }
 app()
